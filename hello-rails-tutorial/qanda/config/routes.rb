5a1665ec52b15878701ac4a7792d9a7b673f6080
JIN <a1824jj@aiit.ac.jp>
2020-05-12 06:31:01 +0000

    set root method

--- hello-rails-tutorial/qanda/config/routes.rb
+++ hello-rails-tutorial/qanda/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # get 'questions/new'
   # get 'questions/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'questions#index'
   resources :questions 
 end
